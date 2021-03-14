48318a35114699b3b4e0a719beb2d4493ed2cd56
Mnvss vamsi <vamsi.m07@gmail.com>
2021-03-14 12:57:48 +0530

    Updates

---
+++ precache-manifest.7db6a410664b923b4eca2e29ca16a8ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf067392b40fdb3b8b290ed76ea91424",
+    "url": "/AccountApp/index.html"
+  },
+  {
+    "revision": "91ae9cbc5b32a391995a",
+    "url": "/AccountApp/static/css/main.133a609a.chunk.css"
+  },
+  {
+    "revision": "f2a9d653a1dba2b956e1",
+    "url": "/AccountApp/static/js/2.44bea109.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AccountApp/static/js/2.44bea109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ae9cbc5b32a391995a",
+    "url": "/AccountApp/static/js/main.74cca31f.chunk.js"
+  },
+  {
+    "revision": "5f49ce6457fc7fef5fc9",
+    "url": "/AccountApp/static/js/runtime-main.fae435ae.js"
+  }
+]);
